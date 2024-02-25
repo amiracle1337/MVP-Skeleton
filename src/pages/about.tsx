090c480178bf730fdf107aacefcb37e0d673dc21
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2024-02-25 15:01:20 +0100

    hello

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const AboutPage = () => {
+  return (
+    <div>
+      <h1>about</h1>
+      <p>The base name of the file being edited, that is, the file name without its extensions.</p>
+    </div>
+  )
+}
+
+export default AboutPage
