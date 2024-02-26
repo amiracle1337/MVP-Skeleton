2e0ace87d8d1b7db197075b22960bd04abe34d41
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2024-02-26 09:36:23 +0100

    fix basic layout issues

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,20 +1,52 @@
 import Head from "next/head"
-import React, { FC } from "react"
+import React from "react"
 import { BlitzLayout } from "@blitzjs/next"
 import { Suspense } from "react"
+import { Group, Flex, Text, Center } from "@mantine/core"
+import { AppShell } from "@mantine/core"
 
 const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
   title,
   children,
 }) => {
+  const thisYear = new Date().getFullYear()
+
   return (
     <>
       <Head>
         <title>{title || "evApp"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 10,
+          breakpoint: "sm",
+        }}
+        padding="md"
+      >
+        <AppShell.Header
+          p={"10px"}
+          style={{ height: 45, display: "flex", alignItems: "center", justifyContent: "left" }}
+        >
+          <div>Eventio</div>
+        </AppShell.Header>
 
-      <Suspense fallback="Loading...">{children}</Suspense>
+        <AppShell.Main>
+          <Suspense fallback="Loading...">
+            <Group> {children}</Group>
+          </Suspense>
+        </AppShell.Main>
+
+        <AppShell.Footer p="md">
+          <Flex justify={"center"} align={"center"}>
+            <Text c="dimmed" fz="xs">
+              copywright {thisYear}
+            </Text>
+          </Flex>
+        </AppShell.Footer>
+      </AppShell>
+      );
     </>
   )
 }
