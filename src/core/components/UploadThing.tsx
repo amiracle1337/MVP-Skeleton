f383bb9ec684b5a72f6ddde0612a98adfe507b99
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2024-04-06 23:26:55 +0200

    mantine styling of change profile pic feature

--- src/core/components/UploadThing.tsx
+++ src/core/components/UploadThing.tsx
@@ -1,5 +1,7 @@
 import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react"
 import { OurFileRouter } from "src/uploadThing/uploadthing-router"
+import { generateReactHelpers } from "@uploadthing/react"
 
 export const UploadButton = generateUploadButton<OurFileRouter>()
 export const UploadDropzone = generateUploadDropzone<OurFileRouter>()
+export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>()
--- src/features/users/forms/EditProfileForm.tsx
+++ src/features/users/forms/EditProfileForm.tsx
@@ -1,15 +1,34 @@
-import { Button, TextInput, Textarea, Flex } from "@mantine/core"
+import { Button, TextInput, Textarea, Flex, FileInput, Text, Group, Loader } from "@mantine/core"
 import { Form, UseFormReturnType } from "@mantine/form"
 import { UpdateProfileInputType } from "src/features/users/schemas"
-import React from "react"
-import { UploadButton } from "src/core/components/UploadThing"
+import React, { useState } from "react"
 import { notifications } from "@mantine/notifications"
+import { IconPhoto } from "@tabler/icons-react"
+import { useUploadThing } from "src/core/components/UploadThing"
+import useBoolean from "react-hanger/array/useBoolean"
 
 export const EditProfileForm: React.FC<{
   form: UseFormReturnType<UpdateProfileInputType>
   onSubmit: (values: UpdateProfileInputType) => void
   isSubmitting: boolean
 }> = ({ onSubmit, form, isSubmitting }) => {
+  const [isLoading, setIsLoading] = useState(false) // Manage loading state with useState
+
+  const { startUpload, permittedFileInfo } = useUploadThing("imageUploader", {
+    onClientUploadComplete: (files) => {
+      setIsLoading(false)
+      const fileKey = files?.[0]?.key
+      fileKey && form.setFieldValue("avatarImageKey", fileKey)
+    },
+    onUploadError: () => {
+      setIsLoading(false)
+      notifications.show({
+        title: "Upload failed",
+        message: "Your file could not be uploaded",
+        color: "red",
+      })
+    },
+  })
   return (
     <Form form={form} onSubmit={onSubmit}>
       <Flex direction="column" gap={15}>
@@ -37,33 +56,30 @@ export const EditProfileForm: React.FC<{
           {...form.getInputProps("bio")}
           radius="md"
         />
-        <Button disabled={!form.isValid()} loading={isSubmitting} type="submit">
-          Submit
-        </Button>
       </Flex>
-      <UploadButton
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          const fileKey = res?.[0]?.key
-
-          console.log("Files: ", res)
-          notifications.show({
-            color: "green",
-            title: "Success!",
-            message: "File uploaded!",
-          })
-          form.setFieldValue("avatarImageKey", fileKey)
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          console.error("Error uploading file: ", error)
-          notifications.show({
-            color: "red",
-            title: "Error!",
-            message: "Failed to upload file. Please try again.",
-          })
+      <FileInput
+        label={
+          <Group m={3}>
+            <Text>Profile picture</Text>
+            {isLoading && <Loader size="xs" />}
+          </Group>
+        }
+        mt={15}
+        style={{ width: "35%" }}
+        placeholder="Profile picture"
+        leftSection={<IconPhoto size={20} />}
+        clearable={true}
+        disabled={isLoading} // Use the isLoading state for the disabled prop
+        onChange={(files) => {
+          setIsLoading(true) // Enable loading state when a file is selected
+          if (files) {
+            startUpload([files]) // Adjust this as needed to match the expected format
+          }
         }}
       />
+      <Button mt={15} disabled={!form.isValid()} loading={isSubmitting} type="submit">
+        Submit
+      </Button>
     </Form>
   )
 }
