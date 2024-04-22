e1187aa166ecf85c8a7ca0b5f5f1ad59cc7d23a3
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2024-04-22 21:34:34 +0200

    introduced chunking for emails in prod using lodash

--- src/features/email/mutations/sendBulkEmail.ts
+++ src/features/email/mutations/sendBulkEmail.ts
@@ -1,11 +1,9 @@
 import { resolver } from "@blitzjs/rpc"
 import { z } from "zod"
-import { sendEmail } from "mailers/sendEmail"
-import React from "react"
 import db from "db"
-import EmailTemplateDummy from "mailers/react-email-starter/emails/dummy"
-import { generateUnsubscribeLink } from "src/utils/email-utils"
+import { chunk } from "lodash"
 import { EmailList } from "src/features/email/types"
+import { isDev } from "src/config"
 
 const Input = z.object({
   list: z.nativeEnum(EmailList),
@@ -23,6 +21,25 @@ export default resolver.pipe(
     if (!user) throw new Error("User not found")
 
     console.log("list iss", list)
+
+    const users = await db.user.findMany({
+      where: {
+        AND: {
+          ...(list === EmailList.Product && { settingsEmailMarketingProduct: true }),
+          ...(list === EmailList.Marketing && { settingsEmailMarketing: true }),
+        },
+        id: {
+          not: user.id,
+        },
+      },
+    })
+
+    console.log("send email to", users.length)
+
+    let CHUNK_SIZE = isDev ? 3 : 100
+    const chunks = chunk(users, CHUNK_SIZE)
+    console.log(chunks)
+
     // let unsubscribeLink = await generateUnsubscribeLink(user.id, user.email)
 
     // await sendEmail({
