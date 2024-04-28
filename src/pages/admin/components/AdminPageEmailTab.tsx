f943e397c0eea300521e1b19ca52e816d9c49939
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2024-04-28 22:28:51 +0200

    added bulk email template preview

--- src/pages/admin/components/AdminPageEmailTab.tsx
+++ src/pages/admin/components/AdminPageEmailTab.tsx
@@ -1,9 +1,10 @@
 import { useMutation } from "@blitzjs/rpc"
-import { Stack, Select } from "@mantine/core"
+import { Stack, Select, Group } from "@mantine/core"
 import sendBulkEmail from "src/features/email/mutations/sendBulkEmail"
 import { EmailList, EmailTemplate } from "src/features/email/types"
 import { Button } from "@mantine/core"
 import { useState } from "react"
+import { EmailTemplates } from "src/features/email/templates"
 
 const listOptions = [
   { value: EmailList.Marketing, label: "Marketing" },
@@ -17,36 +18,43 @@ const templateOptions = [
 ]
 
 export const AdminPageEmailTab = () => {
-  const [test] = useMutation(sendBulkEmail)
   const [list, setList] = useState<EmailList>(EmailList.Marketing)
   const [template, settemplate] = useState<EmailTemplate>(EmailTemplate.Promotion)
+  const [test] = useMutation(sendBulkEmail)
+
+  const foundTemplate = EmailTemplates.find((t) => t.value === template)
+
   return (
-    <Stack>
-      <Select
-        label="Choose email list"
-        placeholder="Pick value"
-        data={listOptions}
-        value={list}
-        onChange={(value) => {
-          setList(value as EmailList)
-        }}
-      />
-      <Select
-        label="Choose email template"
-        placeholder="Pick one"
-        data={templateOptions}
-        value={template}
-        onChange={(value) => {
-          settemplate(value as EmailTemplate)
-        }}
-      />
-      <Button
-        onClick={async () => {
-          await test({ template, list })
-        }}
-      >
-        Send bulk email
-      </Button>
-    </Stack>
+    <div style={{ display: "flex", flexDirection: "row", width: "100vw", gap: "20px" }}>
+      <Stack>
+        <Select
+          label="Choose email list"
+          placeholder="Pick value"
+          data={listOptions}
+          value={list}
+          onChange={(value) => {
+            setList(value as EmailList)
+          }}
+        />
+        <Select
+          label="Choose email template"
+          placeholder="Pick one"
+          data={templateOptions}
+          value={template}
+          onChange={(value) => {
+            settemplate(value as EmailTemplate)
+          }}
+        />
+        <Button
+          onClick={async () => {
+            await test({ template, list })
+          }}
+        >
+          Send bulk email
+        </Button>
+      </Stack>
+      {/* @ts-ignore */}
+      {foundTemplate && <foundTemplate.component />}
+    </div>
   )
 }
