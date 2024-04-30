70d656518afeb4342fa234d39180334185c814ca
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2024-04-30 22:46:28 +0200

    migration prod issue v4

--- db/migrations/20240330150957_email_verified_at/migration.sql
+++ db/migrations/20240330150957_email_verified_at/migration.sql
@@ -1,5 +1,5 @@
 -- AlterEnum
-ALTER TYPE "TokenType" ADD VALUE 'VERIFY_EMAIL';
+-- ALTER TYPE "TokenType" ADD VALUE 'VERIFY_EMAIL';
 
 -- AlterTable
 -- ALTER TABLE "User" ADD COLUMN     "emailVerifiedAt" TIMESTAMP(3);
