107db7a64eefca03d9c7648f4abd6d3f9a71dc84
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2024-05-09 20:35:15 +0200

    Update index.page.ts

--- src/pages/api/lemon/index.page.ts
+++ src/pages/api/lemon/index.page.ts
@@ -11,11 +11,13 @@ export const config = {
   },
 }
 
+// main function trying to handle the webhook request
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   console.log("🍋: hello")
 
   console.log("req.method", req.method)
 
+  // start by verifying the request method
   if (req.method !== "POST") {
     console.log("🍋: method not allowed")
     return res.status(405).json({
@@ -26,7 +28,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   console.log("req.method is allowed")
 
   try {
+    // takes the request which is in node binary and turns it into JSON
     const rawBody = await getRawBody(req)
+    // checks if the hook is valid by sending it to the validateLemonSqueezyHook function
     const isValidHook = await validateLemonSqueezyHook({ req, rawBody })
 
     console.log("🍋: isValidHook", isValidHook)
@@ -37,15 +41,20 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       })
     }
 
+    // ahaaaa! So the handler list is to map out a pre-defined list of what eventTypes
+    // should have what functions. Then we get the eventType from the payload, and based on what it matched, it uses that function
+
     //@ts-ignore
     const event: ResBody["body"] = JSON.parse(rawBody)
     const eventType = event.meta.event_name
     console.log("🍋: event type", eventType)
 
+    // associate the event type "OrderCreated" with onOrderCreated function.
     const handlers = {
       [LemonEventType.OrderCreated]: onOrderCreated,
     }
 
+    // set a variable to equal the handler for the event type name
     const foundHandler = handlers[eventType]
 
     if (foundHandler) {
