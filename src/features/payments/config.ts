3cf96870488bcdd6c5f39e7a83a8b25a524df0ba
Its Amiracle <122039464+amiracle1337@users.noreply.github.com>
2024-05-20 22:47:49 +0200

    added next_public to dynamically render variand plan id's

--- src/features/payments/config.ts
+++ src/features/payments/config.ts
@@ -1,25 +1,25 @@
+import { env } from "src/env.mjs"
+
 export const paymentPlans = [
   {
-    variantId: "383861",
+    variantId: env.NEXT_PUBLIC_LEMONSQUEEZY_MONTHLY_PLAN_VARIANT_ID,
     name: "monthly",
     amount: 99,
     description: "Billed monthly",
   },
   {
-    variantId: "383859",
+    variantId: env.NEXT_PUBLIC_LEMONSQUEEZY_ANNUAL_PLAN_VARIANT_ID,
     name: "annually",
     amount: 999,
     description: "Billed annually",
   },
   {
-    variantId: "383860",
+    variantId: env.NEXT_PUBLIC_LEMONSQUEEZY_LIFETIME_PLAN_VARIANT_ID,
     name: "lifetime",
     amount: 3999,
     description: "One-time payment",
   },
 ]
-
-import { env } from "src/env.mjs"
 
 // export const paymentPlans = [
 //   {
